66b799f086d80318d540be8bc109c03054450e47
Dianeis <mdianeispontes@gmail.com>
2022-09-05 20:46:48 -0300

    Criando atela home

---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import react from "react";
+
+import{
+      View,
+      Text, 
+      TextInput,
+      TouchableOpacity, 
+      StyleSheet
+    } from 'react-native';
+export default function Home(){
+    return(
+        <view>
+            <Text>Meus Filmes</Text>
+            <Text>Olá Dianeis, boa noite!</Text>
+
+           
+        </view>
+
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
+
